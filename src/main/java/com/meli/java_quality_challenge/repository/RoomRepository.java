0d968d48a699f7218a5a8fcfa32ad5392d9e8850
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-04-01 15:22:57 -0300

    add repository RoomRepository

---
+++ src/main/java/com/meli/java_quality_challenge/repository/RoomRepository.java
@@ -0,0 +1,39 @@
+package com.meli.java_quality_challenge.repository;
+
+import com.meli.java_quality_challenge.model.Room;
+
+import java.util.List;
+import java.util.UUID;
+
+public class RoomRepository implements IRepository<Room> {
+
+    @Override
+    public List<Room> create(List<Room> type) {
+        return null;
+    }
+
+    @Override
+    public Room create(Room type) {
+        return null;
+    }
+
+    @Override
+    public List<Room> read(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Room read() {
+        return null;
+    }
+
+    @Override
+    public Boolean update(Room type) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(UUID id) {
+        return null;
+    }
+}
