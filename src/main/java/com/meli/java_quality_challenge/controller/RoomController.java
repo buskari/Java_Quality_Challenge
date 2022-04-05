e2f1947326d05d8ecde1699cea9d199c89460733
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-04-05 12:12:18 -0300

    add GET method do retrieve all rooms

--- src/main/java/com/meli/java_quality_challenge/controller/RoomController.java
+++ src/main/java/com/meli/java_quality_challenge/controller/RoomController.java
@@ -4,6 +4,7 @@ import com.meli.java_quality_challenge.model.Room;
 import com.meli.java_quality_challenge.service.RoomService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class RoomController {
     @PostMapping("/api/v1/room")
     public ResponseEntity<List<Room>> saveRoom(@RequestBody List<Room> roomList) {
         return new ResponseEntity<>(service.saveRoom(roomList), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/v1/room")
+    public ResponseEntity<List<Room>> getRoom() {
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
 }
